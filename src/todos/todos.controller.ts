98826a498ced4371455db64fc6f7f19847e2976a
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-04-19 15:02:24 +0200

    On crée une interface

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import {TodosService} from "./todos.service";
+import {Todo} from "./interfaces/todo.interface";
 
 @Controller('todos')
 export class TodosController {
     constructor(private readonly todosService: TodosService) {}
     @Get()
-    findAll(): any[]{
+    findAll(): Todo[]{
         return this.todosService.findAll();
+    }
+    @Post()
+    createToto(@Body() newTodo){
+        console.log('newTodo', newTodo);
+        this.todosService.create(newTodo);
     }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import {Todo} from "./interfaces/todo.interface";
 
 @Injectable()
 export class TodosService {
-    todos = [
+    todos:Todo[] = [
         {
             id: 1,
             title: 'todos app',
-            description: 'Create NestJS todos app'
+            description: 'Create NestJS todos app',
+            done: false,
         },
         {
             id: 2,
             title: 'bread',
             description: 'Buy some breads',
+            done: true,
         },
+        {
+            id:3,
+            title: 'wine',
+            description: 'buy wine',
+            done: false,
+        }
     ]
-    findAll(): any[]{
+
+    findAll(): Todo[]{
         return this.todos;
+    }
+
+    create(todo: Todo){
+        this.todos = [...this.todos, todo];
     }
 }
